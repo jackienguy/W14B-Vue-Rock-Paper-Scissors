ddf25de50864b463b9a7b18e056bfafb781753ce
Your Name <you@example.com>
2021-08-20 07:06:03 -0600

    Added Login component and GamePage views

---
+++ rps/src/components/LoginPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+        <button @click="loginToGame">Login</button>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import cookies from "vue-cookies"
+
+    export default {
+        name: "LoginPage",
+        methods: {
+            loginToGame(){
+                axios.request({
+                    url: "https://reqres.in/",
+                    method: "POST",
+                     headers: {
+                       'Content-Type': 'application/json', 
+                    },
+                    data: {
+                        "email": "eve.holt@reqres.in",
+                        "password": "cityslicka"
+                    }
+                }).then((response) => {
+                    cookies.set ('loginToken', response.data.token);
+                    console.log(response.data.token);
+                }).catch ((error) => {
+                    console.error("Something went wrong, pleae try login again"+error);
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
